53ac67b01fdabd41d735f063590ab590a8b9da1f
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2019-06-07 19:20:19 +0200

    Precaching other HTML pages

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
+/*
+ * ROUTING
+ */
+
 workbox.routing.registerRoute(
   /\.js$/,
   new workbox.strategies.NetworkFirst({
@@ -39,3 +43,18 @@ workbox.routing.registerRoute(
   })
 );
 
+/*
+ * PRECACHING
+ */
+
+workbox.precaching.precacheAndRoute([
+  { url: '/about/', revision: '20190607' },
+  { url: '/contact/', revision: '20190607' },
+  { url: '/native-lazy-loading-with-vanilla-lazyload/', revision: '20190607' },
+  { url: '/detect-intersection-observer-online-tool/', revision: '20190607' },
+  { url: '/tables-look-like-lists-look-like-tables-accessible-responsive-design/', revision: '20190607' },
+  { url: '/lazy-load-responsive-images-in-2019-srcset-sizes-more/', revision: '20190607' },
+  { url: '/check-if-element-still-inside-viewport-after-given-time/', revision: '20190607' },
+  { url: '/using-css-variables-to-scale-layout-spaces/', revision: '20190607' },
+  { url: '/responsive-images-a-html-51-standard/', revision: '20190607' },
+]);
