2918267533ef414a11cb98251b946665dd00beb0
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2019-06-07 05:43:19 +0200

    Added a simple service worker

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+  /\.js$/,
+  new workbox.strategies.NetworkFirst({
+      cacheName: "js-cache",
+  })
+);
+    
+workbox.routing.registerRoute(
+  /\.css$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'css-cache', 
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.js$/,
+  new workbox.strategies.NetworkFirst({
+      cacheName: "js-cache",
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 20,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  /\/[^\/]+\/$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'posts-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 3,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+
