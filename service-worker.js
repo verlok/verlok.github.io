fcc28637c60a2a8ecd6e0ebb35526a58f8597bd6
verlok <andrea.verlicchi@gmail.com>
2019-06-08 06:49:00 +0200

    Turned off posts precaching

--- service-worker.js
+++ service-worker.js
@@ -4,19 +4,12 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
  * PRECACHING
  */
 
-workbox.precaching.precacheAndRoute([
+// TURNED OFF BECAUSE THESE RESOURCES WOULD BE CACHE-FIRST ONLY!
+/*workbox.precaching.precacheAndRoute([
+  { url: '/', revision: '20190607' },
   { url: '/about/', revision: '20190607' },
-  { url: '/contact/', revision: '20190607' },
-  { url: '/native-lazy-loading-with-vanilla-lazyload/', revision: '20190607' },
-  { url: '/detect-intersection-observer-online-tool/', revision: '20190607' },
-  { url: '/tables-look-like-lists-look-like-tables-accessible-responsive-design/', revision: '20190607' },
-  { url: '/lazy-load-responsive-images-in-2019-srcset-sizes-more/', revision: '20190607' },
-  { url: '/check-if-element-still-inside-viewport-after-given-time/', revision: '20190607' },
-  { url: '/using-css-variables-to-scale-layout-spaces/', revision: '20190607' },
-  { url: '/responsive-images-a-html-51-standard/', revision: '20190607' },
-  { url: '/hybrid-lazy-loading-smashing-magazine-article/', revision: '20190607' },
-]);
-
+  { url: '/contact/', revision: '20190607' }
+]);*/
 
 /*
  * ROUTING
